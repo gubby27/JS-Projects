e5751bcfd9970b3dac610e46eb78195eaf02c611
Kailash Maurya <150253184+gubby27@users.noreply.github.com>
2024-01-09 17:26:26 +0000

    Business Name Generator

---
+++ P2_Business_Name_Generator/BNG.js
@@ -0,0 +1,40 @@
+let rand = Math.random();
+let first, second, third;
+
+// First
+if(rand<0.33){
+    first = "Crazy";
+}
+else if(rand >= 0.33 && rand < 0.66){
+    first = "Amazing";
+}
+else{
+    first = "Fire";
+}
+
+// Second
+rand = Math.random();
+if(rand<0.33){
+    second = "Engine";
+}
+else if(rand >= 0.33 && rand < 0.66){
+    second = "Food";
+}
+else{
+    second = "Garments";
+}
+
+// Third
+rand = Math.random();
+if(rand<0.33){
+    third ="Bros";
+}
+else if(rand >= 0.33 && rand < 0.66){
+    third ="Limited";
+}
+else{
+    third ="Hub";
+}
+
+
+console.log(`${first} ${second} ${third}`);
